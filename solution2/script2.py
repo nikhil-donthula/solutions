7f480445c1eb557d5bf08f0b7f172cea3602df54
nikhil-donthula <donthulanikhil@gmail.com>
2024-04-05 16:24:45 +0530

    script2 to get spcific key value

--- solution2/script2.py
+++ solution2/script2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-
+import base64
 import json
-
+import re
+import subprocess
+import sys
 
 import requests
 
@@ -32,10 +34,39 @@ def main():
     print("Instance provider data:")
     pretty_print_json_obj_to_terminal(instance_json)
 
+    key = sys.argv[1] #"sample_key_for_now"
+    value =  get_value(instance_json, key)
+    if value is not None:
+        print(f"The value for key '{key}' is: {value}")
+    else:
+         print(f"Key '{key}' not found in the JSON data.")
+
 
 def pretty_print_json_obj_to_terminal(json_obj):
     print(json.dumps(json_obj, sort_keys=True, indent=4, separators=(',', ': ')))
 
 
+def get_value(json_data, key):
+  if isinstance(json_data, dict):    
+    if key in json_data:
+      return json_data[key]
+    
+    for value in json_data.values():
+      
+      result = get_value(value, key)
+      if result is not None:
+        return result
+  elif isinstance(json_data, list):  
+    for item in json_data: 
+      result = get_value(item, key)
+      if result is not None:
+        return result
+  return None
+
 if __name__ == "__main__":
     main()
+
+
+
+
+#sample command : python3 sam.py location
